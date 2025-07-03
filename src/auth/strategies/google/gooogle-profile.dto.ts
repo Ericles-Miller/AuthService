8f14bf2f9844bd82b257e754cfd669402d53286f
Ericles Miller <ericlesmiller15@gmail.com>
2025-07-02 22:15:49 -0300

    feat: create method to receiver google token

---
+++ src/auth/strategies/google/gooogle-profile.dto.ts
@@ -0,0 +1,9 @@
+export class GoogleProfileDto {
+  id: string;
+  name: {
+    givenName: string;
+    familyName: string;
+  };
+  emails: Array<{ value: string }>;
+  photos: Array<{ value: string }>;
+}
